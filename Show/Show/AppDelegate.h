36b1111a87f81993edeec728e3bd6a75e3ae9238
Mvp-X <paomoxiaohuai@163.com>
2016-05-23 15:09:43 +0800

    Creat project

---
+++ Show/Show/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Show
+//
+//  Created by Colorful on 16/5/23.
+//  Copyright © 2016年 Mvp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
